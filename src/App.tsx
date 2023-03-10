b808afcfc8535df9da7cc513cd14b76a7c045a22
Indar Basto <indar.basto@gmail.com>
2023-03-10 18:36:07 +0300

    feat: add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,41 @@
-import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import React from 'react';
+import './scss/app.scss';
+import logoIcon from './assets/icons/logo-icon.png';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <header className="header">
+      <div className="logo">
+        <img className="logo__img" alt="Logo" src={logoIcon} />
+        <h1 className="logo__title">
+          Beautiful
+          <br />
+          places
+        </h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="search-bar">
+        <div className="search-bar__wrapper">
+          <input type="text" className="search-bar__input" placeholder="Search anything..." />
+          <button className="search-bar__submit">
+            <i className="fas fa-search"></i>
+          </button>
+        </div>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
+      <nav className="nav">
+        <ul className="nav__list">
+          <li className="nav__item">
+            <a href="#" className="nav__link">
+              Home
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#" className="nav__link">
+              About Us
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
