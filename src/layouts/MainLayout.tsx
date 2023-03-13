5d468e6a058cd917794cfb29cb2f69796248bc02
Indar Basto <indar.basto@gmail.com>
2023-03-13 15:29:59 +0300

    fix: remove unused styles

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header';
 
-import styles from './MainLayout.module.scss';
-
 class MainLayout extends React.Component {
   render() {
     return (
-      <div className={styles.layout}>
+      <React.Fragment>
         <Header />
-        <main className={styles.main}>
+        <main>
           <Outlet />
         </main>
-      </div>
+      </React.Fragment>
     );
   }
 }
