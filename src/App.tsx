c4820aaa47990342d638c2cfd96af75d9513127c
Indar Basto <indar.basto@gmail.com>
2023-03-13 17:53:45 +0300

    feat: home content section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './scss/app.scss';
-// import Header from './components/Header';
 
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
