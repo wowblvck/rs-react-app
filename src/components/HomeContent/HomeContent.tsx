c07f649493edf6eb4d09bdd1a3752fa245d87a1e
Indar Basto <indar.basto@gmail.com>
2023-03-14 18:14:28 +0300

    feat: rewrite loading database

--- src/components/HomeContent/HomeContent.tsx
+++ src/components/HomeContent/HomeContent.tsx
@@ -1,17 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './HomeContent.module.scss';
 import CardItem from '../CardItem/CardItem';
 import { PlacesInfo } from '../../interfaces/Places.interface';
-import data from '../../db/db.json';
 
-class HomeContent extends Component {
+interface HomeContentState {
+  items: PlacesInfo[];
+}
+
+class HomeContent extends React.Component<object, HomeContentState> {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      items: [],
+    };
+  }
+  componentDidMount = async () => {
+    const data = await this.fetchPlaces();
+    this.setState({ items: data });
+  };
+
+  fetchPlaces = async (): Promise<PlacesInfo[]> => {
+    try {
+      const response = await fetch('/db.json');
+      return await response.json();
+    } catch (e) {
+      throw new Error(`Error while loading database: ${e}`);
+    }
+  };
+
   render() {
     return (
       <section className={styles.homeContent}>
         <h3 className={styles.homeContent__title}>Find your place</h3>
         <div className={styles.container}>
           <ul className={styles.cardsList}>
-            {data.map((item: PlacesInfo) => (
+            {this.state.items.map((item: PlacesInfo) => (
               <CardItem key={item.id} obj={item} />
             ))}
           </ul>
