5ea177e78c654d047e0285574118a95d2cdcd092
Indar Basto <indar.basto@gmail.com>
2023-03-14 20:05:39 +0300

    fix: remove main layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,24 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './scss/app.scss';
 
-import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
 import AboutPage from './pages/AboutPage';
+import Header from './components/Header/Header';
 
 class App extends React.Component {
   render() {
     return (
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route path="" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Route>
-      </Routes>
+      <React.Fragment>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </main>
+      </React.Fragment>
     );
   }
 }
