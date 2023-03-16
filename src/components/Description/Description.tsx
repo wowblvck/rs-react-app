872ae6a0b37b9e4f9b221dca5fcd32c5c14b5402
Indar Basto <indar.basto@gmail.com>
2023-03-16 15:17:42 +0300

    feat: add random background, add background blur

--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import styles from './Description.module.scss';
 import SearchBox from '../SearchBox/SearchBox';
+import { randomBg } from '../../utils/bgRandomizer';
 
-class Description extends React.Component {
+type DescriptionState = {
+  image: string;
+};
+
+class Description extends React.Component<never, DescriptionState> {
+  state = {
+    image: '',
+  };
+  componentDidMount() {
+    const storedImage = sessionStorage.getItem('bgImage');
+    if (storedImage) {
+      this.setState({ image: storedImage });
+    } else {
+      const newImage = randomBg();
+      this.setState({ image: newImage });
+      sessionStorage.setItem('bgImage', newImage);
+    }
+  }
+
   render() {
+    const bgImage = {
+      '--img': `url(${this.state.image}`,
+    } as React.CSSProperties;
+
     return (
-      <section className={styles.description}>
+      <section className={styles.description} style={bgImage}>
         <div className={styles.descriptionContent}>
           <h2 className={styles.descriptionContent__title}>Beautiful Places</h2>
           <p className={styles.descriptionContent__subtitle}>
