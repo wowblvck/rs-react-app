22733e85ff6c0a4be71442e527eff4fe54eaba7c
Indar Basto <indar.basto@gmail.com>
2023-03-18 17:34:42 +0300

    feat: create unit tests

---
+++ src/pages/AboutPage.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import AboutPage from './AboutPage';
+
+describe('AboutPage', () => {
+  it('should render the AboutContent component', () => {
+    const { getByTestId } = render(<AboutPage />);
+    const aboutContent = getByTestId('about-content');
+    expect(aboutContent).toBeInTheDocument();
+  });
+});
