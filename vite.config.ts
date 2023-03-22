2a02819b1e6afdf3645ef06ffd1d287a68e3fee3
Indar Basto <indar.basto@gmail.com>
2023-03-22 12:53:41 +0300

    feat: add FontAwesome react component

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@
 /// <reference types="vite/client" />
 
 import react from '@vitejs/plugin-react';
+import macrosPlugin from 'vite-plugin-babel-macros';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), macrosPlugin()],
   test: {
     globals: true,
     environment: 'jsdom',
