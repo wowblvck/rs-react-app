bc20696edbf890918c084d9bede659b82c622e50
Indar Basto <indar.basto@gmail.com>
2023-03-29 11:03:27 +0300

    feat: add autoprefix plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,16 @@
 import react from '@vitejs/plugin-react';
 import macrosPlugin from 'vite-plugin-babel-macros';
 import { defineConfig } from 'vite';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), macrosPlugin()],
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
