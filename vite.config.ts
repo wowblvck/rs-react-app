5cf6f510f12d6201c8c40afca1bca7fb45506ff3
Indar Basto <indar.basto@gmail.com>
2023-03-31 10:21:25 +0300

    feat: add image optimization plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@
 
 import react from '@vitejs/plugin-react';
 import macrosPlugin from 'vite-plugin-babel-macros';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { DEFAULT_OPTIONS } from './imageOptimization.config';
 import { defineConfig } from 'vite';
 import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), macrosPlugin()],
+  plugins: [react(), macrosPlugin(), ViteImageOptimizer(DEFAULT_OPTIONS)],
   css: {
     postcss: {
       plugins: [autoprefixer()],
