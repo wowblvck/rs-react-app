2b87c0fd3b5ae7c05d20c69499926bf1880081cb
Indar Basto <indar.basto@gmail.com>
2023-04-14 19:33:25 +0300

    fix: tests

--- src/components/CardItem/CardItem.test.tsx
+++ src/components/CardItem/CardItem.test.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import CardItem from './CardItem';
 import { mockData } from '../../tests/mocks/mockData';
+import { Provider } from 'react-redux';
+import store from '../../store/store';
+import CardPopup from '../CardPopup/CardPopup';
 
 describe('CardItem', () => {
   test('renders correctly with provided data', async () => {
@@ -18,5 +21,22 @@ describe('CardItem', () => {
       )
     ).toHaveLength(1);
     expect(screen.getAllByText(mockData[0].date)).toHaveLength(1);
+  });
+
+  test('call popup on click', async () => {
+    const onClose = vi.fn();
+    render(
+      <Provider store={store}>
+        <CardItem obj={mockData[0]} />
+        <CardPopup itemId={mockData[0].id} onClose={onClose} />
+      </Provider>
+    );
+
+    const item = screen.getByTestId('card-item');
+    fireEvent.click(item);
+
+    await waitFor(() => {
+      expect(screen.getByText(mockData[0].location)).toBeInTheDocument();
+    });
   });
 });
