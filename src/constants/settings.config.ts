b4a4c36c75681da28ba8acd658e65a354c9ef387
Indar Basto <indar.basto@gmail.com>
2023-05-01 13:30:08 +0300

    fix: api key value

--- src/constants/settings.config.ts
+++ src/constants/settings.config.ts
@@ -2,7 +2,7 @@ const URL = 'https://enthusiastic-wasp-jacket.cyclic.app';
 
 const PROXY = 'https://important-crab-cardigan.cyclic.app';
 const URL_IMAGE = `${PROXY}/https://freeimage.host/api/1/upload`;
-const API_KEY = import.meta.env.VITE_API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY || '6d207e02198a847aa98d0a2a901485a5';
 
 enum URLPath {
   Places = 'places',
