736abe286e42a8bf50f2ab8356daa367fd95c3c2
Indar Basto <indar.basto@gmail.com>
2023-08-03 14:05:30 +0300

    Revert "feat: add random background, add background blur"
    This reverts commit 802becaf1dbbd035d9dc739b3e915a8964d93cf7.

--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -1,34 +1,11 @@
 import React from 'react';
 import styles from './Description.module.scss';
 import SearchBox from '../SearchBox/SearchBox';
-import { randomBg } from '../../utils/bgRandomizer';
 
-type DescriptionState = {
-  image: string;
-};
-
-class Description extends React.Component<never, DescriptionState> {
-  state = {
-    image: '',
-  };
-  componentDidMount() {
-    const storedImage = sessionStorage.getItem('bgImage');
-    if (storedImage) {
-      this.setState({ image: storedImage });
-    } else {
-      const newImage = randomBg();
-      this.setState({ image: newImage });
-      sessionStorage.setItem('bgImage', newImage);
-    }
-  }
-
+class Description extends React.Component {
   render() {
-    const bgImage = {
-      '--img': `url(${this.state.image}`,
-    } as React.CSSProperties;
-
     return (
-      <section className={styles.description} style={bgImage}>
+      <section className={styles.description}>
         <div className={styles.descriptionContent}>
           <h2 className={styles.descriptionContent__title}>Beautiful Places</h2>
           <p className={styles.descriptionContent__subtitle}>
--- src/utils/bgRandomizer.tsx
+++
@@ -1,5 +0,0 @@
-import { bgGallery } from '../App';
-
-export const randomBg = () => {
-  return bgGallery[Math.floor(Math.random() * bgGallery.length)];
-};
