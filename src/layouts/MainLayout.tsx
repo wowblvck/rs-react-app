34aff505f192cb37b18509d7c3b382b8fac4dba2
Indar Basto <indar.basto@gmail.com>
2023-08-03 14:05:30 +0300

    Revert "fix: remove unused styles"
    This reverts commit f2f6d6a8665cb285eed510c5c1b8fc4bb6bc15d5.

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header';
 
+import styles from './MainLayout.module.scss';
+
 class MainLayout extends React.Component {
   render() {
     return (
-      <React.Fragment>
+      <div className={styles.layout}>
         <Header />
-        <main>
+        <main className={styles.main}>
           <Outlet />
         </main>
-      </React.Fragment>
+      </div>
     );
   }
 }
