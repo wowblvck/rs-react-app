3ef01af073f39dc54cc8959c8143ba31c5295642
Indar Basto <indar.basto@gmail.com>
2023-08-03 14:05:30 +0300

    Revert "feat: rewrite loading database"
    This reverts commit 826ca89593ab071bf5762f7dac5a454a9634926d.

--- src/components/HomeContent/HomeContent.tsx
+++ src/components/HomeContent/HomeContent.tsx
@@ -1,40 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './HomeContent.module.scss';
 import CardItem from '../CardItem/CardItem';
 import { PlacesInfo } from '../../interfaces/Places.interface';
+import data from '../../db/db.json';
 
-interface HomeContentState {
-  items: PlacesInfo[];
-}
-
-class HomeContent extends React.Component<object, HomeContentState> {
-  constructor(props: object) {
-    super(props);
-    this.state = {
-      items: [],
-    };
-  }
-  componentDidMount = async () => {
-    const data = await this.fetchPlaces();
-    this.setState({ items: data });
-  };
-
-  fetchPlaces = async (): Promise<PlacesInfo[]> => {
-    try {
-      const response = await fetch('/db.json');
-      return await response.json();
-    } catch (e) {
-      throw new Error(`Error while loading database: ${e}`);
-    }
-  };
-
+class HomeContent extends Component {
   render() {
     return (
       <section className={styles.homeContent}>
         <h3 className={styles.homeContent__title}>Find your place</h3>
         <div className={styles.container}>
           <ul className={styles.cardsList}>
-            {this.state.items.map((item: PlacesInfo) => (
+            {data.map((item: PlacesInfo) => (
               <CardItem key={item.id} obj={item} />
             ))}
           </ul>
